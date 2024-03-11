b8e444a28acbf6c2e92106b042343431ea168af7
JakIzy03 <161471073+JakIzy03@users.noreply.github.com>
2024-03-11 19:48:16 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 const FULLNAME = "Jakub Izydorczyk";
 const STUDENT_ID = 42881;
 console.log(`My name is${FULLNAME}. Mystudent ID is ${STUDENT_ID}`);
+
+const getStudentFullName = () => FULLNAME;
+const getStudentId = () => STUDENT_ID;
